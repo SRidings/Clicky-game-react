10f1f941525f9a3a227a652ec21f56a45cb97c03
Robert Castiblanco <ccrs2006@gmail.com>
2017-12-10 22:08:48 -0700

    first commit, should be adding features later

---
+++ src/components/Title/Title.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Title.css";
+
+const Title = props => (
+    <div className="title">
+        <h1>GAME INSTRUCTIONS:</h1>
+        <h2>Click on the images but never click on the same one twice.</h2>
+        <h2>Or you will lose!</h2>
+    </div>
+);
+  
+
+export default Title;
+
+
